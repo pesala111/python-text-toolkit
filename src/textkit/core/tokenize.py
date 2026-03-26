585c1ab2c06449df7e84558403a06563a5030209
pesala111 <pesalamadhavareddy1@gmail.com>
2026-03-26 01:18:09 +0100

    fix: use built-in list type hints

--- src/textkit/core/tokenize.py
+++ src/textkit/core/tokenize.py
@@ -1,9 +1,7 @@
 import re
-from typing import List
 
 _TOKEN = re.compile(r"\w+|[^\w\s]", re.UNICODE)
 
-
-def simple_tokenize(text: str) -> List[str]:
+def simple_tokenize(text: str) -> list[str]:
     """Tokenize into words and punctuation."""
     return _TOKEN.findall(text)
